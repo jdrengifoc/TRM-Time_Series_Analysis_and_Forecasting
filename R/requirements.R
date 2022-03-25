2599b4c2a64922ca28aab7ac58bc91d81207db0e
jdrengifoc <41349362+jdrengifoc@users.noreply.github.com>
2022-03-25 17:30:42 -0500

    Create requirements.R

---
+++ R/requirements.R
@@ -0,0 +1,16 @@
+# Installation or load of libraries
+packages <- c("devtools",                      # R-Packages
+              "readxl", "writexl", "openxlsx", # Excel files
+              "stats", "tseries",              # Timeseries
+              "tidyverse"                      # Escentials
+              )
+# Install and load packages.              
+if (all(packages %in% rownames(installed.packages()))){
+  lapply(packages, library, character.only = T)
+} else {
+  idx <- which((packages %in% rownames(installed.packages()))==F)
+  install.packages(packages[idx])
+  lapply(packages, library, character.only = T)
+}
+# Remove unused variables.
+rm(base_dir, packages)
